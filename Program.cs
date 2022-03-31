54775dc7697477b7dec5e958c27caa72f9638a59
elyadesu <elyadesu@yandex.ru>
2022-03-31 16:44:18 +0300

    Minor Bug Fixes + added exception handler

--- Program.cs
+++ Program.cs
@@ -10,17 +10,33 @@ namespace DLLResourceReader
 	{
 		static void Main(string[] args)
 		{
-			var path = @"D:\Documents\C\REPOS\DLLResourceReader\DLLResourceReader\bin\Debug\DWGConverterCP.dll"; // Your path to dll file
-			var assembly = Assembly.ReflectionOnlyLoadFrom(path);
-			var names = assembly.GetManifestResourceNames();
-			for (int i=0; i < names.Count() - 1; i++)
+			var path = @"C:\Users\Admin\Desktop\CefSharp.dll"; // Your path to dll file
+			try
 			{
-				var stream = assembly.GetManifestResourceStream(names[i]);
-				var RR = new ResourceReader(stream);
-				IDictionaryEnumerator dict = RR.GetEnumerator();
-				while (dict.MoveNext())
-					Console.WriteLine("   {0}: '{1}' (Type {2})", dict.Key, dict.Value, dict.Value.GetType().Name); //Output
-				RR.Close();
+				var assembly = Assembly.ReflectionOnlyLoadFrom(path);
+				var names = assembly.GetManifestResourceNames();
+				if (names.Count() == 0)
+				{
+					Console.WriteLine("No Resources");
+					Console.ReadKey();
+				}
+				else
+				{
+					for (int i = 0; i <= names.Count() - 1; i++)
+					{
+						Console.WriteLine("\n\n{0}", names[i]);
+						var stream = assembly.GetManifestResourceStream(names[i]);
+						var RR = new ResourceReader(stream);
+						IDictionaryEnumerator dict = RR.GetEnumerator();
+						while (dict.MoveNext())
+							Console.WriteLine("   {0}: '{1}' (Type {2})", dict.Key, dict.Value, dict.Value.GetType().Name); //Output
+						RR.Close();
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
 			}
 			Console.ReadKey();
 		}
